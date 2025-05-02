285c830354cfffc69619ba0b20d1d4ae3203ea03
Félix Suarez Bonilla <felixdavidsuarezbonilla@gmail.com>
2025-05-02 08:03:51 -0600

    Add files with error

--- src/components/Version.test.jsx
+++ src/components/Version.test.jsx
@@ -7,12 +7,12 @@ import packageJson from '../../package.json';
 
 test('Renders Version', () => {
   render(<Versioning version={packageJson.version} />)
-  const linkElement = screen.getByText(`Current version: ${packageJson.version}`)
+  const linkElement = screen.getByText(`Current version: 9.9.9`)
   expect(linkElement).toBeInTheDocument()
 })
 
 test('Does not render wrong version', () => {
   render(<Versioning version={packageJson.version} />)
-  const wrongText = screen.queryByText('Current version: 9.9.9!')
+  const wrongText = screen.queryByText('Current version: 9.9.9')
   expect(wrongText).not.toBeInTheDocument()
 })
